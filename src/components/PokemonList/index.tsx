7afd3ad01c5a2573bcf901d0ab1362de6fa198ce
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2023-01-18 10:20:46 -0300

    add pagination

--- src/components/PokemonList/index.tsx
+++ src/components/PokemonList/index.tsx
@@ -2,16 +2,18 @@ import styles from "./styles.module.scss";
 import { PokemonCard } from "../PokemonCard";
 interface Props {
 	pokemonsUrls?: string[] | null;
-	page?: number;
-	perPage?: number;
+	page: number;
+	perPage: number;
 }
 
 export const PokemonList = ({ pokemonsUrls, page, perPage }: Props) => {
 	return (
-		<div>
-			{pokemonsUrls?.map((pokemonUrl) => (
-				<PokemonCard key={pokemonUrl} url={pokemonUrl} />
-			))}
+		<div className={styles.pokemons}>
+			{pokemonsUrls
+				?.slice((page - 1) * perPage, (page - 1) * perPage + perPage)
+				.map((pokemonUrl) => (
+					<PokemonCard key={pokemonUrl} url={pokemonUrl} />
+				))}
 		</div>
 	);
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,18 +3,33 @@ import { PokemonContext } from "../../context/PokemonContext";
 import { PokemonList } from "../../components/PokemonList";
 import styles from "./styles.module.scss";
 import { useContext } from "react";
+import { Pagination } from "../../components/Pagination";
+import { usePagination } from "../../hooks/usePagination";
 
 export const Home = () => {
 	const { pokemonsFiltered } = useContext(PokemonContext);
+	const { page, nextPage, previousPage, backToHome } = usePagination();
+	let perPage = 12;
 	return (
 		<div className={styles.home}>
 			<header>
-				<div>
+				<div onClick={backToHome}>
 					<PokeballIconSmall />
 					<span>Pokédex</span>
 				</div>
 			</header>
-			<PokemonList pokemonsUrls={pokemonsFiltered} />
+			<PokemonList
+				page={page}
+				perPage={perPage}
+				pokemonsUrls={pokemonsFiltered}
+			/>
+			<Pagination
+				page={page}
+				perPage={perPage}
+				nextPage={nextPage}
+				previousPage={previousPage}
+				maxItems={pokemonsFiltered?.length!}
+			/>
 		</div>
 	);
 };
